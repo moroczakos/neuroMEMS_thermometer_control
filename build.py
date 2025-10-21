ac8424c23679cd5026ddf09e3c4049c5b95be388
morak <moroczkos@gmail.com>
2025-10-21 17:05:16 +0200

    Remove force includes

--- build.py
+++ build.py
@@ -29,18 +29,6 @@ for pkg in packages:
 
 print(f"\n Total hidden imports collected: {len(hidden_imports)}")
 
-# Force-include any modules PyInstaller tends to miss
-force_includes = [
-    'apps.cycle_sequence_editor',
-]
-
-for m in force_includes:
-    if m not in hidden_imports:
-        hidden_imports.append(m)
-        print(f"Forcing inclusion of {m}")
-    else:
-        print(f"Already detected: {m}")
-
 # Run PyInstaller build
 args = [
     'main.py',
@@ -52,7 +40,6 @@ args = [
     '--exclude=PyQt5',
     '--paths=.',
     *[f'--hidden-import={m}' for m in hidden_imports],
-    '--add-data=apps/cycle_sequence_editor.py;apps',
     '--clean'
 ]
 
