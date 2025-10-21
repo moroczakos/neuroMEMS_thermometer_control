4a14ecaec96573ec3781247e7d0868edb163eaf9
morak <moroczkos@gmail.com>
2025-10-21 17:44:11 +0200

    Remove unused import, .spec files

--- build.py
+++ build.py
@@ -1,7 +1,6 @@
 import os
 import sys
 import PyInstaller.__main__
-from PyInstaller.utils.hooks import collect_submodules
 
 repo_root = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, repo_root)
